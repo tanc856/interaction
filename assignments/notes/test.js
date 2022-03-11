81f67cbbf44e750bb963acc86fd95cbb588bdf62
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-03-11 09:50:24 -0500

    up

---
+++ assignments/notes/test.js
@@ -0,0 +1,8 @@
+const constantVariable = "cannot be changed"
+let dynamicVariable = "can be changed"
+
+const firstName = "Cynthia"
+const lastName = "Tan"
+const fullName = firstName + " " + lastName
+
+console.log(fullName);
