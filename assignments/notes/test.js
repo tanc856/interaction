9386af9628409579d6f0549beeee9ddb26eef65e
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-03-13 15:41:05 -0400

    update

--- assignments/notes/test.js
+++ assignments/notes/test.js
@@ -1,4 +1,4 @@
-const constantVariable = "cannot be changed"
+/*onst constantVariable = "cannot be changed"
 let dynamicVariable = "can be changed"
 
 const firstName = "Cynthia"
@@ -6,3 +6,23 @@ const lastName = "Tan"
 const fullName = firstName + " " + lastName
 
 console.log(fullName);
+
+
+const multiplyByTwo = function(input) {}
+
+const multiplyByTwo = function(numberOne, numberTwo) {}
+
+
+/* loops */
+
+/*let str ="";
+
+for (let index = 0; index < 10; index++) {
+    str = str + index;
+}
+
+console. log (str)*/
+
+
+
+
---
+++ assignments/workshops/workshop_3.js
@@ -0,0 +1,10 @@
+
+
+const clockElement = document.querySelector("h2.clock")
+
+const getCurrentTime = function() {
+    // get current time string 
+
+}
+
+setInterval(getCurrentTime, 1000)
