a8c12c1be137dc0396059a685c0d9a44b56ebe4b
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-03-23 11:42:37 -0400

    update

--- assignments/workshops/workshop_3.js
+++ assignments/workshops/workshop_3.js
@@ -1,10 +1,17 @@
 
-
-const clockElement = document.querySelector("h2.clock")
-
-const getCurrentTime = function() {
-    // get current time string 
-
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setInterval(startTime, 1000);
+    setTimeout(startTime, 1000);
 }
 
-setInterval(getCurrentTime, 1000)
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
