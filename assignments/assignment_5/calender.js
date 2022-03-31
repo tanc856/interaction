bd756b83a8a1f20cf37f6cd788594f59438f3e65
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-03-31 19:20:32 -0400

    calendar update

--- assignments/assignment_5/calender.js
+++ assignments/assignment_5/calender.js
@@ -3,7 +3,7 @@ let i = 0;
 
 function changeByHour() {
   const hour = document.getElementById("hour");
-  const color = ["black", "purple", "yellow", "blue"];
+  const color = ["#96B3C2", "#57838D", "#B4C9C7", "#F3BFB3"];
   hour.style.backgroundColor = color[i];
   i = (i + 1) % color.length;
 }
@@ -13,7 +13,7 @@ setInterval(changeByHour, 1000 * 60 * 60);
 
 function changeByMinute() {
     const minute = document.getElementById("minute");
-    const color = ["black", "purple", "yellow", "blue"];
+    const color = ["#445A67", "#57838D", "#B4C9C7", "#F3BFB3"];
     minute.style.backgroundColor = color[i];
     i = (i + 1) % color.length;
   }
@@ -22,9 +22,23 @@ setInterval(changeByMinute, 1000 * 60);
 
 function changeBySecond() {
     const second = document.getElementById("second");
-    const color = ["black", "purple", "yellow", "blue"];
+    const color = ["#96B3C2", "#57838D", "#B4C9C7", "#F3BFB3", "CCADB2"];
     second.style.backgroundColor = color[i];
     i = (i + 1) % color.length;
   }
   
 setInterval(changeBySecond, 1000);
+
+// adding month and year // 
+
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let name = month[d.getMonth()];
+document.getElementById("month").innerHTML = name;
+
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+
+let date = d.getDate();
+document.getElementById("date").innerHTML = date;
