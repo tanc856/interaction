faa859069baf167fa9fd2a152af8e9fa645c00f4
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-04-08 17:03:59 -0400

    update

---
+++ projects/project3/progress/drafts_1/draft_1.js
@@ -0,0 +1,10 @@
+
+
+const airtableApiKey = "keyOIp9wOnn8B5nk8";
+
+// URL of Where our Data is Located
+const tableUrl = "https://api.airtable.com/v0/app2XuYjq81zafdJb/Raye's%20collection";
+
+// URL with API Key Authentication
+const authenticatedUrl = tableUrl + "?api_key=" + airtableApiKey;
+
