785d52671d53792e89dbb9505bdbcae2da7881a7
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-04-14 19:46:21 -0400

    assignment

---
+++ assignments/assignment_7/script.js
@@ -0,0 +1,56 @@
+// API key
+const airtableApiKey = "keyOIp9wOnn8B5nk8";
+
+// URL of the airtable file
+const airtableURL = "https://api.airtable.com/v0/app2XRcw3r6xY9vwu/Recipe%20randomizer";
+
+// URL with API Key Authentication
+const authenticatedUrl = airtableURL + "?api_key=" + airtableApiKey;
+
+
+// DOM references
+const textElement = document.querySelector('#text')
+const vegetableElement = document.querySelector('#vegetable')
+const proteinElement = document.querySelector('#protein')
+const carbElement = document.querySelector('#carb')
+const mealElement = document.querySelector('#meal')
+
+
+// FUNCTIONS
+
+
+
+// FETCH
+
+fetch(authenticatedUrl)
+  // When 'Promise' is 'Resolved', get the JSON data
+  .then((res) => res.json())
+  // Use the JSON Data
+  .then((data) => {
+    const words = data.records;
+    const meals = words.filter((word) => {
+        return word.fields.meal === 'meal'
+    })
+
+    console.log (data)
+    const vegetables = words.filter((word) => {
+        return word.fields.vegetables === 'vegetable'
+    })
+    const proteins = words.filter((word) => {
+        return word.fields.protein === 'protein'
+    })    
+    const carbs = words.filter((word) => {
+        return word.fields.carb === 'carb'
+    })
+
+    // Set Values onto HTML Elements 
+    vegetableElement.innerHTML = vegetables[Math.floor(Math.random()*vegetables.length)].fields.vegetable
+    proteinElement.innerHTML = proteins[Math.floor(Math.random()*proteins.length)].fields.protein
+    carbElement.innerHTML = carbs[Math.floor(Math.random()*carbs.length)].fields.carb
+    mealElement.innerHTML = meals[Math.floor(Math.random()*meals.length)].fields.meal
+  }
+)
+
+
+
+
