3d496a0a59477ea8ff2a7384b5d42e0f8dcc0c10
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-04-15 11:33:53 -0400

    project3 update

--- projects/project3/progress/finaldraft/cityscape.js
+++ projects/project3/progress/finaldraft/cityscape.js
@@ -19,6 +19,7 @@ const jsonPromise = fetchPromise.then((response) => {
     return response.json()
 })
 
+// FOR LOOP 
 jsonPromise.then((data) => {
     const records = data.records 
     for (let index = 0; index < records.length; index++) {
@@ -42,10 +43,76 @@ jsonPromise.then((data) => {
         backgroundContainerElement.appendChild(backgroundElement)
 }
 
+for (let index = 0; index < records.length; index++) {
+    const attachmentUrl = records[index].fields.Attachments[0].url
+    const numbers = [index]
+    // Create Button Element
+    const buttonElement = document.createElement('button')
+    buttonElement.classList.add('numbers')
+    buttonElement.innerHTML = numbers; 
+    // Create Image Element
+    const imageElement = document.createElement('img')
+    imageElement.classList.add('selected')
+    imageElement.setAttribute('src', attachmentUrl)
+    // Create Background Image Element
+    const backgroundElement = document.createElement('img')
+    backgroundElement.classList.add('selected')
+    backgroundElement.setAttribute('src', attachmentUrl)
+    // Add Image Element to the Container Element
+    buttonsContainerElement.appendChild(buttonElement)
+    photographContainerElement.appendChild(imageElement)
+    backgroundContainerElement.appendChild(backgroundElement)
+}
+
+for (let index = 0; index < records.length; index++) {
+    const attachmentUrl = records[index].fields.Attachments[0].url
+    const numbers = [index]
+    // Create Button Element
+    const buttonElement = document.createElement('button')
+    buttonElement.classList.add('numbers')
+    buttonElement.innerHTML = numbers; 
+    // Create Image Element
+    const imageElement = document.createElement('img')
+    imageElement.classList.add('selected')
+    imageElement.setAttribute('src', attachmentUrl)
+    // Create Background Image Element
+    const backgroundElement = document.createElement('img')
+    backgroundElement.classList.add('selected')
+    backgroundElement.setAttribute('src', attachmentUrl)
+    // Add Image Element to the Container Element
+    buttonsContainerElement.appendChild(buttonElement)
+    photographContainerElement.appendChild(imageElement)
+    backgroundContainerElement.appendChild(backgroundElement)
+}
+
+for (let index = 0; index < records.length; index++) {
+    const attachmentUrl = records[index].fields.Attachments[0].url
+    const numbers = [index]
+    // Create Button Element
+    const buttonElement = document.createElement('button')
+    buttonElement.classList.add('numbers')
+    buttonElement.innerHTML = numbers; 
+    // Create Image Element
+    const imageElement = document.createElement('img')
+    imageElement.classList.add('selected')
+    imageElement.setAttribute('src', attachmentUrl)
+    // Create Background Image Element
+    const backgroundElement = document.createElement('img')
+    backgroundElement.classList.add('selected')
+    backgroundElement.setAttribute('src', attachmentUrl)
+    // Add Image Element to the Container Element
+    buttonsContainerElement.appendChild(buttonElement)
+    photographContainerElement.appendChild(imageElement)
+    backgroundContainerElement.appendChild(backgroundElement)
+}
+
+
 
 // DOM REFERENCES SELECT DYNAMIC CONTENT
 const buttons = document.querySelectorAll('button')
 const images = document.querySelectorAll('img')
+const foregroundImages = document.querySelectorAll('#photograph-container img')
+const backgroundImages = document.querySelectorAll('#background-container img')
 
 
 // FUNCTIONS
@@ -57,12 +124,20 @@ const handleEvents = (event) => {
     // TURN BACK ON BUTTON CORRESPONDING TO BUTTON PRESSED 
     const buttonValue = event.target.innerHTML
     images[buttonValue].classList.add('selected')
-} // Why is it not adding 'selected to the background elements??
-
+    foregroundImages[buttonValue].classList.add('selected')
+    backgroundImages[buttonValue].classList.add('selected')
+}
 
 // ADD BUTTON
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener('click', handleEvents)
     buttons[index].addEventListener('mouseover', handleEvents)
+}
+
+// ADD ABOUT PAGE BUTTON
+document.getElementById("about").addEventListener("mouseover", myFunction);
+
+function myFunction() {
+  document.getElementById("about").innerHTML = "Photograph by Raye Hwangbo &nbsp &nbsp &nbsp &nbsp &nbsp Design by Cynthia Tan";
 }
 })
