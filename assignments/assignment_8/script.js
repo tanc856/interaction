eb2c887cd382fdd47324091361b483fdcebb94d0
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-04-29 16:21:54 -0400

    assignment 8 update

--- assignments/assignment_8/script.js
+++ assignments/assignment_8/script.js
@@ -1,15 +1,41 @@
 
 // DOM references
-const userOutputElement = document.querySelector("#user-output");
-const userInputElement = document.querySelector("form#user-input");
+const userOutputElement1 = document.querySelector("#user-output1");
+const userInputElement1 = document.querySelector("form#user-input1");
+const userOutputElement2 = document.querySelector("#user-output2");
+const userInputElement2 = document.querySelector("form#user-input2");
+const userOutputElement3 = document.querySelector("#user-output3");
+const userInputElement3 = document.querySelector("form#user-input3");
 
 // FUNCTION
-userInputElement.addEventListener("submit", (event) => {
+userInputElement1.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const userInputElement = event.target.querySelector("input");
-    event = userInputElement.value;
+    const userInputElement1 = event.target.querySelector("input");
+    event = userInputElement1.value;
 
     const createListElement = document.createElement("li");
-    (createListElement.innerHTML = event), userOutputElement.appendChild(createListElement), (userInputElement.value = "");
+    (createListElement.innerHTML = event), userOutputElement1.appendChild(createListElement), (userInputElement1.value = "");
 });
+
+userInputElement2.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const userInputElement2= event.target.querySelector("input");
+    event = userInputElement2.value;
+
+    const createListElement = document.createElement("li");
+    (createListElement.innerHTML = event), userOutputElement2.appendChild(createListElement), (userInputElement2.value = "");
+});
+
+userInputElement3.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const userInputElement3 = event.target.querySelector("input");
+    event = userInputElement3.value;
+
+    const createListElement = document.createElement("li");
+    (createListElement.innerHTML = event), userOutputElement3.appendChild(createListElement), (userInputElement3.value = "");
+});
+
+
