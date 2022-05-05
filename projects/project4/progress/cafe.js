021e8a9b2b091f3b631df7bfcd818306e0775246
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-05-05 09:46:36 -0400

    update

--- projects/project4/progress/cafe.js
+++ projects/project4/progress/cafe.js
@@ -1,6 +1,31 @@
 
 // DOM references
 
+
+
+
+
+
+
+
+
+// GREETING
+
+const myDate = new Date();
+const hrs = myDate.getHours();
+
+let greeting;
+
+if (hrs < 12)
+  greeting = 'Good Morning ☀ ';
+else if (hrs >= 12 && hrs <= 17)
+  greeting = 'Good Afternoon! ';
+else if (hrs >= 17 && hrs <= 24)
+  greeting = 'Good Evening ☾ ';
+
+document.getElementById('greeting').innerHTML ='<b>' + greeting + '</b>';
+
+
 // CLOCK
 function startTime() {
     const today = new Date();
