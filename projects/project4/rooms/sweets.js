372983685dc51e9b7a308c0e32aeef0a0b8bc5d1
Cynthia Huiwen Tan <tanc856@newschool.edu>
2022-05-05 00:53:55 -0400

    05/04 update

--- projects/project4/rooms/sweets.js
+++ projects/project4/rooms/sweets.js
@@ -1,5 +1,44 @@
 
+const airtableApiKey = "keyOIp9wOnn8B5nk8";
+
+// URL of Where our Data is Located
+const tableUrl = "https://api.airtable.com/v0/appyCplnJrLOhtyPB/Table%201";
+
+// URL with API Key Authentication
+const authenticatedUrl = tableUrl + "?api_key=" + airtableApiKey;
+
+
 // DOM references
+const nameSubmissionElement = document.querySelector('#name')
+const urlSubmissionElement = document.querySelector('#linkhere')
+
+
+// APPLICATION
+const fetchPromise = fetch(authenticatedUrl)
+const jsonPromise = fetchPromise.then((response) => {
+    return response.json()
+})
+
+
+// FOR LOOP 
+jsonPromise.then((data) => {
+    const records = data.records 
+    console.log (records)
+    for (let index = 0; index < records.length; index++) {
+        const urlLink = records[index].fields.URL
+        // Create Url Response
+        const urlResponseElement = document.createElement('a')
+        if (index >= 0) {
+            urlResponseElement.classList.add('submitedLink')
+            urlResponseElement.setAttribute("href", urlLink);
+            urlResponseElement.innerHTML = urlLink; 
+        }
+       
+        // Add each Element to the Container Element
+        urlSubmissionElement.appendChild(urlResponseElement)
+    }
+})
+
 
 // CLOCK
 function startTime() {
